895fc87a2c00738d3b9bdedd77ab800a0476d28d
Alex Thacker <thacker.alex@yahoo.com>
2019-02-28 14:44:58 -0600

    a

--- src/script/main.js
+++ src/script/main.js
@@ -1,19 +1,20 @@
 // Create an array that contains the words in the sentence
-let sentence = ["The","walrus","danced","through","the","trees","in","the","light",
-"of","the","moon"];
+let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light",
+    "of", "the", "moon"
+];
 
 /*
     The addExcitement function should be an impure function, and accept
     the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-function addExcitement (theWordArray, punct, amount) {
+const addExcitement = (theWordArray, punct, amount) => {
 
     // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
     let punctAmount = ''
 
-    for (let i = 0; i < amount; i++){
+    for (let i = 0; i < amount; i++) {
         punctAmount += punct
     }
 
@@ -21,26 +22,26 @@ function addExcitement (theWordArray, punct, amount) {
     for (let i = 0; i < theWordArray.length; i++) {
         // Concatenate the new word onto buildMeUp
         buildMeUp += ' ' + theWordArray[i]
-        
-        
 
-        if ((i + 1) % 3 === 0 ){
-            
-            
-            buildMeUp +=  punctAmount
-            
-        
+
+
+        if ((i + 1) % 3 === 0) {
+
+
+            buildMeUp += punctAmount
+
+
         }
         console.log(buildMeUp);
-        
-        
-        // Print buildMeUp to the console
-        
-        
-        
-    
 
-}
+
+        // Print buildMeUp to the console
+
+
+
+
+
+    }
 }
 
 // Invoke the function and pass in the array
